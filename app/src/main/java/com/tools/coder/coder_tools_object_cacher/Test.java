27318e4d9a1b860e4463f1bef4989782ba33d19b
xuchun <xuchun@didichuxing.com>
2016-01-04 17:32:13 +0800

    Add lib and demo

---
+++ app/src/main/java/com/tools/coder/coder_tools_object_cacher/Test.java
@@ -0,0 +1,11 @@
+package com.tools.coder.coder_tools_object_cacher;
+
+import java.io.Serializable;
+
+/**
+ * Created by Spring-Xu on 16/1/4.
+ */
+public class Test implements Serializable {
+    public String name;
+    public String value;
+}
